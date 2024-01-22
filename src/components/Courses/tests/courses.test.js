e9bf2b562febec206c5c0ae7474ed0c1dedb5f3d
David <davidgegic95@gmail.com>
2024-01-22 18:56:47 +0100

    module 5 done

--- src/components/Courses/tests/courses.test.js
+++ src/components/Courses/tests/courses.test.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
